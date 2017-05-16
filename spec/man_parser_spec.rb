add4600fd6c1dc2f46151d3040c477eca87d4f4a
san3091 <san3091@gmail.com>
2017-05-16 18:14:41 -0400

    Start specs

--- spec/man_parser_spec.rb
+++ spec/man_parser_spec.rb
@@ -1,4 +1,15 @@
 require "spec_helper"
+require "gimme"
+require "pry"
 
 RSpec.describe Manatee::ManParser do
+  let(:man_page) { `man rake | col -bx` }
+
+  it "parses a man page" do
+    splits_text = gimme_next(SplitsText)
+    groups_into_sections = gimme_next(GroupIntoSections)
+    give(splits_text).split() {}
+    give(groups_into_sections).group() {}
+    Manatee::ManParser.parse(man_page)
+  end
 end
